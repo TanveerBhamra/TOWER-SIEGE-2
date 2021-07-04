7cda53e2d4531f7a9488a1909d8ba602715c360a
TanveerBhamra <81157724+TanveerBhamra@users.noreply.github.com>
2021-07-04 16:49:05 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,17 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      imageMode(CENTER);
+      image(blockImg, pos.x, pos.y);
+    }
+}
